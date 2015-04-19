98f074ea7dfaea917be4b1a1995a29f7bd009e89
Goose <goosemixes@gmail.com>
2015-04-19 00:47:21 -0400

    Small changes
    removed unused file, renamed title.

--- routes/info.js
+++ routes/info.js
@@ -5,7 +5,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('info', { 
-  	title: 'First App',
+  	title: 'info',
 	bar: 'Hello Everyone',
 	supplies: ['mop', 'broom', 'duster']
   });
